7b45423a4d6ba72e34a5a16295dd63dccd4cec31
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2023-03-14 19:18:43 +0530

    Add files via upload

---
+++ Filtering Paths/routes/admin.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const rootDir = require('../util/path');
+
+const express = require('express');
+
+const router = express.Router();
+
+
+// router.get('/add-product', (req, res, next) => {
+//     res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+
+// })
+
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+})
+
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
