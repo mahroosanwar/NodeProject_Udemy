862cd8397bda257408336773df7980d0bffc9dc4
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2023-03-21 13:35:50 +0530

    Add files via upload

---
+++ Connecting to the MongoDB Server with Mongoose/app.js
@@ -0,0 +1,61 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+
+const app = express();
+
+const mongoose = require("mongoose");
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const User = require("./models/user");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("6416ea50949c2312a8fcc452")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    "mongodb+srv://mahroosanwar:Onetwo12@cluster0.dhavdaa.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Mahroos",
+          email: "test@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    console.log("Connected!");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
