39e0e8003b9f185f42a3e5e599bbec5dbccc5065
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2023-03-23 04:46:31 +0530

    Add files via upload

---
+++ The Current Project Status/controllers/auth.js
@@ -0,0 +1,34 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[0];
+
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  // res.setHeader('Set-Cookie', "loggedIn=true: HttpOnly")
+  User.findById("641af5e84d7513ccf4b260c1")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect("/");
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
