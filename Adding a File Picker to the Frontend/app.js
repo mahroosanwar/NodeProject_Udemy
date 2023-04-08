a007c98711b0a3fab34a01a4c5ae42f8d6bbe00b
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2023-04-09 00:07:03 +0530

    Add files via upload

---
+++ Adding a File Picker to the Frontend/app.js
@@ -0,0 +1,128 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+const session = require("express-session");
+const MongoDbStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const multer = require("multer");
+const { v4: uuidv4 } = require('uuid');
+
+const MONGODB_URI =
+  "mongodb+srv://mahroosanwar:Onetwo12@cluster0.ezgn0kf.mongodb.net/shop?retryWrites=true&w=majority";
+
+const app = express();
+const store = new MongoDbStore({
+  uri: MONGODB_URI,
+  collecton: "sessions",
+});
+
+const mongoose = require("mongoose");
+
+const csrfProtection = csrf();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const User = require("./models/user");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    // cb(null, new Date().toISOString() + "-" + file.originalname);
+    cb(null, uuidv4() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use("/500", errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.redirect('/500');
+  res.status(500).render("500", {
+    pageTitle: "Error Occured",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
+
+mongoose
+  .connect(
+    MONGODB_URI
+    // { useNewUrlParser: true }
+  )
+  .then((result) => {
+    console.log("Connected!");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
